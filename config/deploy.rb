683f0dd36f2d6af779dea9ee9bc5752888e64fef
kazkosa <kazu_kosa@icloud.com>
2020-10-16 05:45:37 +0900

    Update Capistrano Setting 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.14.1"
 # Application Name
 set :application, "taskleaf2"
 # Setting Github
-set :repo_url, "git@example.com:kazkosa/taskleaf2.git"
+set :repo_url, "git@github.com:kazkosa/taskleaf2.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
